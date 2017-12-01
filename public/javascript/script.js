abd636ac4aa4ba4499356700610f060d3e57b001
Thomas Charuel <thomascharuel@hotmail.fr>
2017-12-01 11:38:56 +0100

    add to newsletter list

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,28 +1,39 @@
+function isValidEmailAddress(emailAddress) {
+  var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+  return pattern.test(emailAddress);
+};
+
 $(document).ready(function(){
 
 
   $('.newsletterForm').submit(function(e){
     e.preventDefault();
 
-    // Set up a new API client
-    // Comes from the SDK generated with AWS API Gateway
-    var client = apigClientFactory.newClient();
+    // Get email value
+    var email = $(this).children('input[type="email"]').val();
 
-    // The SDK contains helper methods for all of the API endpoints we created.
-    // Here we are using the subscribePost method which talks to the /subscribe endpoint
-    // The second parameter is the body of our request, so here we will capture the email
-    // provided and make the call to the API
-    client.subscribePost({}, {email: $('#email').val()}, {})
-      .then(function(data) {
-        console.dir(data)
-        // If all went well, we'll display a success message, otherwise we'll display an error
-        // Our Lambda response is wrapped in API Gateway's own response object, that is why we are 
-        // accessing data.data object
-        if(data.data.statusCode == 200){
-          alert('Add to newsletter list')
-        } else {
-          alert('error add newsletter list')
+    if(email && email != "" && isValidEmailAddress(email)){
+      // Set up a new API client
+      // Comes from the SDK generated with AWS API Gateway
+      var client = apigClientFactory.newClient();
+    
+      // The SDK contains helper methods for all of the API endpoints we created.
+      // Here we are using the subscribePost method which talks to the /subscribe endpoint
+      // The second parameter is the body of our request, so here we will capture the email
+      // provided and make the call to the API
+      client.subscribePost({}, {email: $('#email').val()}, {})
+        .then(function(data) {
+          console.dir(data)
+          // If all went well, we'll display a success message, otherwise we'll display an error
+          // Our Lambda response is wrapped in API Gateway's own response object, that is why we are 
+          // accessing data.data object
+          if(data.data.statusCode == 200){
+            alert('Add to newsletter list')
+          } else {
+            alert('error add newsletter list')
+          }
         }
-      })
+      )
+    }
   });
 });
